0996cc6d8b8c4b397e9fee982dec85088a1af2c0
RyanCool09345 <thien025254@stu.vinschool.edu.vn>
2026-04-07 20:49:00 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,46 @@
-const CACHE_NAME="todo-app-v1";
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(["./","./index.html"])));
+const CACHE_NAME = "todo-app-v2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+// INSTALL (cache files)
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));
+
+// ACTIVATE (delete old caches)
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH (cache-first, then network)
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).then(networkRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, networkRes.clone());
+          return networkRes;
+        });
+      });
+    })
+  );
 });
